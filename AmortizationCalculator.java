cd2804cdf2bad114d9662bea8106faf11be3ac08
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-21 22:19:25 +0200

    AmortizationCalculator final

--- AmortizationCalculator.java
+++ AmortizationCalculator.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
 
 public class AmortizationCalculator {
   private static Scanner scanner = new Scanner(System.in);
@@ -18,6 +21,7 @@ public class AmortizationCalculator {
     System.out.printf("Your periodic payment amount is %.4f\n", amortization);
     System.out.println("=====================================================");
 
+    printTable(principal, numberOfPayments);
   }
 
   private static double calculate(double principal, int numberOfPayments){
@@ -30,5 +34,65 @@ public class AmortizationCalculator {
       amortization = principal / numberOfPayments;
     }
     return amortization;
+  }
+
+  private static void printTable(double principal, int numberOfPayments){
+    // Begin printing table
+    Object[] headers = {"Payment #", "Amount Due", "Interest Due", "Principal Due", "Amount Left"};
+    System.out.println("\n=================Your Mortgage Payoff Table============================");
+    System.out.println("\n=================--------------------------============================");
+    System.out.format("%5s %12s %18s %16s \n", headers);
+    System.out.println("\n=======================================================================");
+
+    //Establish values to display
+    double monthlyPayment = calculate(principal, numberOfPayments);
+    double interestPayment = 0.0;
+    double principalDue = 0.0;
+    double principalLeft = principal;
+    boolean loanPaid = false;
+
+    Object[][] rows = new Object[numberOfPayments][];
+    for(int i = 0; i < numberOfPayments; i++) {
+      interestPayment = monthlyRate * principalLeft;
+      principalDue = monthlyPayment - interestPayment;
+      principalLeft = Math.abs(principalLeft - principalDue);
+      Object[] row = {i+1, monthlyPayment, interestPayment, principalDue, principalLeft};
+      rows[i] = row;
+      System.out.format("%5d%17.4f%19.4f%19.4f%17.4f\n", row);
+    }
+
+    promptToSaveCSV(rows);
+  }
+
+  private static void promptToSaveCSV(Object[][] rows){
+    System.out.println("Would you like to save the results to a CSV file? [Yes/No]:");
+    String response = scanner.next();
+
+    if ("yes".equalsIgnoreCase(response)) {
+      try{
+        FileWriter fileWriter = new FileWriter("amortization_table.csv");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(Object[] row: rows){
+          for(Object column: row){
+            if (stringBuilder.length() != 0){
+              stringBuilder.append(',');
+            }
+            stringBuilder.append(column);
+          }
+          stringBuilder.append("\n");
+          bufferedWriter.write(stringBuilder.toString());
+          stringBuilder.setLength(0);
+        }
+        bufferedWriter.close();
+
+        System.out.println("Results saved to amortization_table.csv");
+      } catch(IOException e){
+        e.printStackTrace();
+      }
+    } else {
+      System.out.println("You chose not to save results in a CSV file.");
+    }
   }
 }
